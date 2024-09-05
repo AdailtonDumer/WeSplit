44906e4539bb3803779111929afb7eb723fe053c
Adailton Lucas <57913921+AdailtonDumer@users.noreply.github.com>
2024-09-05 12:23:35 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Adailton Lucas on 05/09/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
